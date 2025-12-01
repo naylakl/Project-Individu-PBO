967a132026fa35d6556b6c9e1f32ba0a594bb349
Nayla Khansa Livya <naylakl2406@gmail.com>
2025-12-01 17:54:44 +0700

    Add constructor to Penumpang class

--- Penumpang.java
+++ Penumpang.java
@@ -4,3 +4,11 @@ public abstract class Penumpang implements Pembayaran {
     protected boolean hamil;
     protected int saldo;
     protected Kategori kategori;
+
+    public Penumpang(int id, int umur, boolean hamil) {
+        this.id = id;
+        this.umur = umur;
+        this.hamil = hamil;
+        this.saldo = 10000;
+        tentukanKategori();
+    }
