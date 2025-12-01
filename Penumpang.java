d71babe390f7b968910d0f47d7a0323640598f5d
Nayla Khansa Livya <naylakl2406@gmail.com>
2025-12-01 17:55:08 +0700

    Update Penumpang.java

--- Penumpang.java
+++ Penumpang.java
@@ -12,3 +12,10 @@ public abstract class Penumpang implements Pembayaran {
         this.saldo = 10000;
         tentukanKategori();
     }
+
+    private void tentukanKategori() {
+        if (hamil) kategori = Kategori.IBU_HAMIL;
+        else if (umur < 10) kategori = Kategori.ANAK;
+        else if (umur > 60) kategori = Kategori.LANSIA;
+        else kategori = Kategori.BIASA;
+    }
