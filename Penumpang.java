bdeef165dbda9dc23ba79379e46f9248a58ecc23
Nayla Khansa Livya <naylakl2406@gmail.com>
2025-12-01 17:54:09 +0700

    Create abstract Penumpang class

---
+++ Penumpang.java
@@ -0,0 +1,6 @@
+public abstract class Penumpang implements Pembayaran {
+    protected int id;
+    protected int umur;
+    protected boolean hamil;
+    protected int saldo;
+    protected Kategori kategori;
