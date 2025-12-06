87c6734ba8931e7cc8c8738f0a7dc050d7fce75f
Nayla Khansa Livya <naylakl2406@gmail.com>
2025-12-06 11:05:01 +0700

    Update Bus.java

--- Bus.java
+++ Bus.java
@@ -1,1 +1,15 @@
+import java.util.ArrayList;
 
+public class Bus {
+    private ArrayList<Penumpang> prioritas;
+    private ArrayList<Penumpang> biasa;
+    private ArrayList<Penumpang> berdiri;
+
+    public static final int ONGKOS = 2000;
+    private int pendapatan = 0;
+
+    public Bus() {
+        prioritas = new ArrayList<>();
+        biasa = new ArrayList<>();
+        berdiri = new ArrayList<>();
+    }
